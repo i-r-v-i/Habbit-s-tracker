b9d281e5f99a86779b9e15a522c4db903658b3fd
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-03-28 16:18:04 +0300

    feat: Добавлен ререндер меню на моковых данных

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,55 @@
 let habbits = [];
-const HABBIT_KEY = 'HABBIT_KEY'
+const HABBIT_KEY = "HABBIT_KEY";
+
+const page = {
+    menu: document.querySelector('.menu__list')
+}
 
 function loadData() {
-  const habbitString = localStorage.getItem('HABBIT_KEY');
+  const habbitString = localStorage.getItem("HABBIT_KEY");
   const habbitArray = JSON.parse(habbitString);
-  if(Array.isArray(habbitArray)) {
+  if (Array.isArray(habbitArray)) {
     habbits = habbitArray;
   }
 }
 
 function saveData() {
-    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits))
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
-(()=>{
-    loadData()
+// render
+function rerenderMenu(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+  for (const habbit of habbits) {
+    const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+    if (!existed) {
+        const element = document.createElement('button');
+        element.setAttribute('menu-habbit-id', habbit.id);
+        element.classList.add('menu__item');
+        element.addEventListener('click', ()=> rerender(habbit.id))
+        element.innerHTML = `<image src="./assets/svg/${habbit.icon}.svg" alt="${habbit.name}" />`;
+        if(activeHabbit.id === habbit.id) {
+          element.classList.add('menu__item_active');
+        }    
+        page.menu.appendChild(element);
+        continue;
+    }
+    if(activeHabbit.id === habbit.id) {
+        existed.classList.add('menu__item_active');
+    } else {
+        existed.classList.remove('menu__item_active');
+    }
+  }
+}
+
+function rerender(activeHabbitId) {
+  const activeHabbit = habbits.find((habbit) => (habbit.id === activeHabbitId));
+  rerenderMenu(activeHabbit);
+}
+
+(() => {
+  loadData();
+  rerender(habbits[0].id)
 })();
